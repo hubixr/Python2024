e8919b55ce688131a81838d0cd1813f1e8515393
hubixr <hubert.migafka@gmail.com>
2025-02-10 16:33:07 +0100

    Poprawki

--- lab9/dashboard.py
+++ lab9/dashboard.py
@@ -24,23 +24,18 @@ initial_conditions = [S0, I0, R0]
 # Czas symulacji w dniach
 t = np.linspace(0, 360, 360) # 360 dni co 1 dzien
 
-# Function to solve the SIR model
 def solve_sir(beta, gamma):
     solution = odeint(sir_model, initial_conditions, t, args=(beta, gamma))
     S, I, R = solution.T
     return S, I, R
 
-# Initial beta and gamma
 beta = 0.3
 gamma = 0.1
-
-# Solve the model with initial parameters
 S, I, R = solve_sir(beta, gamma)
 
-# Create ColumnDataSource
 source = ColumnDataSource(data={'t': t, 'S': S, 'I': I, 'R': R})
 
-# Create plot
+#wykres
 plot = figure(title='SIR Model', x_axis_label='Time (days)', y_axis_label='Proportion of Population', height=400, width=700)
 plot.line('t', 'S', source=source, legend_label='Podatni', color='blue')
 plot.line('t', 'I', source=source, legend_label='Zarażeni', color='red')
@@ -48,24 +43,21 @@ plot.line('t', 'R', source=source, legend_label='Martwi/Wyzdrowiali', color='gre
 plot.legend.location = 'center_right'
 plot.legend.click_policy = 'hide'
 
-# Sliders for beta and gamma
+# Slidery
 beta_slider = Slider(start=0.1, end=1.0, value=beta, step=0.01, title='Beta')
 gamma_slider = Slider(start=0.05, end=0.5, value=gamma, step=0.01, title='Gamma')
 
-# Update function
+
 def update(attr, old, new):
     beta = beta_slider.value
     gamma = gamma_slider.value
     S, I, R = solve_sir(beta, gamma)
     source.data = {'t': t, 'S': S, 'I': I, 'R': R}
 
-# Attach update function to sliders
 beta_slider.on_change('value', update)
 gamma_slider.on_change('value', update)
 
-# Layout
-layout = column(beta_slider, gamma_slider, plot)
 
-# Add layout to current document
+layout = column(beta_slider, gamma_slider, plot)
 curdoc().add_root(layout)
 curdoc().title = 'SIR Model Dashboard'
